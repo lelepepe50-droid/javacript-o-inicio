c6ae480fccf3b964cebc363dafc5e2141e6b5bba
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2025-11-03 22:26:35 -0300

    boas -braticas-case-sensitive
    Declaração de variáveis(var, let, const)

---
+++ aula001/app.js
@@ -0,0 +1,67 @@
+// Boas práticas
+/*visam melhorar a legibilidade, manutenção e qualidade do código, além de evitar erros comuns.*/
+
+
+// Case sensitive
+/* o que significa que a capitalização diferencia maiúsculas de minúsculas. Isso se aplica a nomes de variávei */
+console.log("olá mundo!")
+
+// Sintaxe básica e comentário
+/*
+//uma linha
+/*
+varias
+linhas
+*/
+
+// Declaração de variáveis(var, let, const)
+let nome ="leandro"
+console.log(" olá " + nome)
+nome = "andrade"
+console.log(nome)
+
+const idade = 19
+console.log( "tenho",idade)
+/*são usados para declarar variáveis, mas diferem principalmente em escopo (onde a variável é acessível) e mutabilidade (se o valor pode ser alterado).*/
+
+// Console e Debug
+/* é o processo de encontrar e corrigir erros, conhecidos como "bugs", em programas de computador, aplicativos ou hardware*/
+
+//Tipos de dados
+/*
+inteiro - int
+real - float
+booleano - bool
+texto - string
+*/
+//string
+let cidade = "americana"
+
+//Number
+let salario = "1000"
+
+//bool
+let fumante = false
+
+console.log(typeof cidade)
+console.log(typeof salario)
+console.log(typeof fumante)
+
+
+// Operadores
+//     Operador Atribuição
+
+//     Operadores aritméticos
+
+//     Operadores relacionais
+
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+
+//     Laços de repetição
+
+// Arrays --> vetor
+
+// Funções personalizadas
