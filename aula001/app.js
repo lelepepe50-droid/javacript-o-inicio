cf4faea721259d0b8a4d2dd724a93b51dc9cf2ec
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2025-11-05 22:22:07 -0300

    novo javacript
    nova javacript

--- aula001/app.js
+++ aula001/app.js
@@ -47,21 +47,101 @@ console.log(typeof cidade)
 console.log(typeof salario)
 console.log(typeof fumante)
 
+console.log("-----------")
+salario="2000"
+console.log( typeof salario)
+
 
 // Operadores
-//     Operador Atribuição
+//     Operador Atribuição ( = )
+salario = 2500
 
-//     Operadores aritméticos
+//     Operadores aritméticos ( +,-,*,/,% )
+let n1 = 10, n2= 5
+console.log(n1+n2)//soma
+console.log(n1-n2)//subtração
+console.log(n1*n2)//mutiplicação
+console.log(n1/n2)//divisão
+console.log(10%3) //módulo 
 
-//     Operadores relacionais
 
-//     Operadores lógicos
+//     Operadores relacionais(==,!=,>,<,>=,<=)
+console.log(n1 == n2) // igual/igualdade
+console.log(n1 != n2)// diferente
+console.log(n1 >= n2)//maior
+console.log(n1 <= n2)//menor ou igual
 
+
+//     Operadores lógicos !-NÃO|NOT ||-OU/QR e|AND
+let altura = 1.7
+let peso = 79
+let tipo = "criança"
+
+//muda o resultado final, se for verdadeiro vira falso e se falso vira verdadeira
+console.log(altura > 1.5 || peso < 150 || tipo != "criança")
+
+// com || apenas uma verificação precisa ser verdadeira
+console.log(altura > 1.5 || peso < 150 || tipo !="criança")
+
+
+// com && todas as virificações devem ser verdeira para o resuldatos final ser vedardeiro
+console.log(altura > 1.5 && peso <150 && tipo != "criança")
+
+
+ 
 // Estrutura
-//     Estrutura de controle/decisão
+// Estrutura de controle/decisão
+let carro ="fiat palio"
 
-//     Laços de repetição
+if(carro == "parati"){
+    console.log("vc ta bem mais ou menos")//verdadeiro
+}else{
+    console.log("a vida ta progredindo")//falso
+}
+
+let idadealuno = 20
+if(idadealuno >= 18){
+    console.log("maior de idade")
+}
+else{
+    console.log("menor de idade")
+}
+
+// Laços de repetição
+let controle = 1
+while(controle <= 10){
+    console.log(7*controle)
+    controle = controle + 1
+}
 
 // Arrays --> vetor
+let zoologico = ['leão','elefante','sebra','ariranha','lobo guará', 'arara']
+console.log (zoologico [2])
+console.log (zoologico.length)
+console.log("zoologico")
+
+let frutas = ['🍓','🍊','🍍','🍌','🍇','🍉']
+console.log(frutas)
+frutas.push('🍎')
+console.log(frutas)
+
+let alunos = []
+alunos.push("joão")
+alunos.push("leandro")
+alunos.push("roberto")
+console.log(alunos)
 
 // Funções personalizadas
+function mensagem(){
+    console.log("olá")
+    console.log("seja bem vindo")
+    console.log(":)")
+}
+mensagem()
+mensagem()
+
+function conta(horas,sal_hora){
+    console.log(horas * sal_hora)
+}
+
+Conta(140, 75)
