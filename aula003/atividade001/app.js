a0714b44eb0d66bd974b9ad5877fc0c4b905bd06
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2025-11-10 20:16:44 -0300

    tudo sobre javacript

---
+++ aula003/atividade001/app.js
@@ -0,0 +1,12 @@
+let n1 = 0, n2 = 0, n3 = 0, media = 0
+n1= 7
+n2 = 5.4
+n3 = 3.27
+n4 = 8
+media = (n1 + n2 + n3 + n4) / 4
+
+console.log("média="+media.toFixed(2))
+
+// 1- como fazer para o resultado apresentar apenas 2 casas decimal
+
+// 2- como fazer para o progama solicitar as notas para o usuario
