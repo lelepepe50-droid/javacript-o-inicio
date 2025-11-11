bfe15f2e691f05505e6b7e302c916827e6819259
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2025-11-10 22:23:14 -0300

    atividades003

---
+++ aula003/atividade003/app.js
@@ -0,0 +1,15 @@
+function fnjogo(){
+    let chute = 0, numeromagico = Math.floor(Math.random)() * 10 + 1;
+    chute = document.getElementById("valor").valeu
+
+    if(chute == numeromagico){
+        document.getElementById("resultado"). innerText ="acertou 😊"
+      }else{
+        Document.getElementById("resultado").innerText = "errou 😓"  
+    }
+}
+
+// 1 - como fazer um sorteio de número de 1 a 10 ao invés de  deixar fixo o 3
+
+//if verdadeiro
+// else falso 
