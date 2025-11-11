6adce397b6f5305fc0eacc15a2889dec0912b0f0
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2025-11-10 21:44:17 -0300

    valor dolar e valor em euro

---
+++ aula003/atividade002/app.js
@@ -0,0 +1,14 @@
+function fnConverterValor(){
+  let valordigitado = 0, total= 0, totaleuro = 0
+  valordigitadov = document.getRootNode(valor).valeu
+  total = valordigitado * 5.29
+   total = valordigitado *6.11
+  document.getElementById("valorconvertido").innerText="valor em dolar" + total
+   document.getElementById("valorconvertido").innerText="valor em euro" + total
+
+}
+
+// 1 - como colocar o resultado no formato R$ XX.XX
+
+// 2 - como mostrar o resultado da conversão de dólar e euro
+
